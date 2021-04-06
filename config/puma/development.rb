34ebad6033c4263521947b222798c4df89cc567a
Matija <matkatic@gmail.com>
2021-04-07 01:17:01 +0200

    ssl enabled

--- config/puma/development.rb
+++ config/puma/development.rb
@@ -4,8 +4,19 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
 threads threads_count, threads_count
 
-port        ENV.fetch("PORT") { 3000 }
+port        ENV.fetch("PORT") { 3001 }
 environment "development"
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+# # Add ssl certificate
+server_key = "#{Rails.application.secrets.server_key}"
+server_crt = "#{Rails.application.secrets.server_crt}"
+
+# To be able to use rake etc
+ssl_bind '127.0.0.1', 3000, {
+key: server_key,
+cert: server_crt,
+# verify_mode: 'none'
+}
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -2,4 +2,16 @@
 
 _load_from File.expand_path("../defaults.rb", __FILE__)
 
+port        ENV.fetch("PORT") { 3001 }
 environment "production"
+
+# # Add ssl certificate
+server_key = "#{Rails.application.secrets.server_key}"
+server_crt = "#{Rails.application.secrets.server_crt}"
+
+# To be able to use rake etc
+ssl_bind '127.0.0.1', 3000, {
+key: server_key,
+cert: server_crt,
+# verify_mode: 'none'
+}
