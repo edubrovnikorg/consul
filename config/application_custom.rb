a019e2b8c0bf122f6fef838dea8a2bd66b2805b6
Matija <matkatic@gmail.com>
2021-04-14 20:27:41 +0200

    secret key fix

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,13 @@
 module Consul
   class Application < Rails::Application
+    def secret_key_base 
+      if Rails.env.test? || Rails.env.development? 
+        secrets.secret_key_base || Digest::MD5.hexdigest(self.class.name) 
+      else 
+        validate_secret_key_base( 
+          ENV["SECRET_KEY_BASE"] || credentials.secret_key_base || secrets.secret_key_base 
+        ) 
+      end 
+    end 
   end
 end
