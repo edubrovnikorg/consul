b014e8e4e8de3654af273c107fb32c214acce716
Matija <matkatic@gmail.com>
2021-04-14 21:16:33 +0200

    fix key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Secret key manual insert.
+  config.secret_key_base = '<%= ENV["secret_key_base"] %>'
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -97,4 +100,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
 end
