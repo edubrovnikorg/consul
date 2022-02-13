dde35ff763ead463cee75ca29cb5e2d24afcafc6
Eidar <matkatic@gmail.com>
2022-02-13 15:25:48 +0100

    prijevodi

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,10 @@
-set :branch, ENV["branch"] || :master
+set :branch, ENV["branch"] || 'consul-api'
+set :ssh_options, {
+  user: "deploy",
+  forward_agent: false,
+  keys: '~/.ssh/consul_matija',
+  auth_methods: %w(publickey password)
+}
 
 server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
