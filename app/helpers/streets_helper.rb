8634aa1e07cd4b3a2c1206cdb1c9b9727d6e3f62
Eidar <matkatic@gmail.com>
2022-12-18 22:30:15 +0100

    helper fix

--- app/helpers/streets_helper.rb
+++ app/helpers/streets_helper.rb
@@ -2,15 +2,17 @@ module StreetsHelper
   def street_filter_helper(current_user, poll)
     result = true
 
-    poll.streets.each do |street|
-      street_name = street.name.downcase
-      user_address = current_user.address.downcase
-      user_address = user_address.gsub!(/[[:space:]]\d+[a-z]*/, "")
-      unless street_name.include?(user_address) || user_address.include?(street_name)
-        result = false
-      else
-        result = true
-        break
+    if current_user.address.present?
+      poll.streets.each do |street|
+        street_name = street.name.downcase
+        user_address = current_user.address.downcase
+        user_address = user_address.gsub!(/[[:space:]]\d+[a-z]*/, "")
+        unless street_name.include?(user_address) || user_address.include?(street_name)
+          result = false
+        else
+          result = true
+          break
+        end
       end
     end
 
