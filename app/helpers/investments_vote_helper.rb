bcfd13498c91c4aa99eb8d741b3d2861d09299ea
Eidar <matkatic@gmail.com>
2023-09-24 23:05:45 +0200

    result percentages with two decimal places

--- app/helpers/investments_vote_helper.rb
+++ app/helpers/investments_vote_helper.rb
@@ -1,7 +1,7 @@
 module InvestmentsVoteHelper
   def votes_to_percent(votes, total_votes)
     return 0 if votes.zero? || total_votes.zero?
-    return ((votes.to_f / total_votes.to_f) * 100).round(0);
+    return '%.2f' % ((votes.to_f / total_votes.to_f) * 100);
   end
 end
 
