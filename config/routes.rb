9231e389a55ef63b9e8593749907abac1f924a10
Eidar <matkatic@gmail.com>
2024-06-10 15:54:32 +0200

    design and route fix

--- config/routes.rb
+++ config/routes.rb
@@ -37,8 +37,8 @@ Rails.application.routes.draw do
   root "budgets#index"
   get "/welcome", to: "welcome#welcome"
   get "/consul.json", to: "installation#details"
-  get "maintenance#index", controller: "maintenance", action: "index", as: "maintenance"
-  post "maintenance#login", controller: "maintenance", action: "login", as: "maintenance_login"
+  get "/odrzavanje-sustava", controller: "maintenance", action: "index", as: "maintenance"
+  post "/odrzavanje-sustava", controller: "maintenance", action: "login", as: "maintenance_login"
 
   resources :stats, only: [:index]
   resources :images, only: [:destroy]
